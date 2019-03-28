41913eecc61a190555495bed49fef6fe93f8b538
Julia <julia.feec@gmail.com>
2019-03-28 16:51:02 -0700

    add svg overlay

--- webm/flask_app_camera/app.py
+++ webm/flask_app_camera/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/static/<path:path>')
+def send_js(path):
+    return send_from_directory('static', path)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
--- webm/flask_app_camera/start_ffmpeg.sh
+++ webm/flask_app_camera/start_ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -video_size 160x128 -i /dev/video0 \
+ffmpeg -f v4l2 -i /dev/video0 \
        -f lavfi -i sine \
        -c:v libvpx -threads 4 -speed 6 -pix_fmt yuv420p -b:v 512k -r 30 \
        -c:a libopus -threads 4 -b:a 256k -ac 2 -ar 48000 \
