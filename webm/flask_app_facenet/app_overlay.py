62d7fc83990aab5ce501fed1cf007e7519ad084d
Ubuntu <ubuntu@ip-172-31-22-141.us-west-1.compute.internal>
2019-04-10 03:20:28 +0000

    add facenet changes

--- webm/flask_app_facenet/app_overlay.py
+++ webm/flask_app_facenet/app_overlay.py
@@ -31,4 +31,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
---
+++ webm/flask_app_facenet/start_ffmpeg_remote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#ffmpeg -f v4l2 -i /dev/video0 \
+#73.241.109.34
+#ffmpeg -rtsp_transport tcp -i rtsp://73.241.109.34:8554/unicast \
+#ffmpeg -i rtsp://73.241.109.34:8554/unicast \
+#ffmpeg -i rtsp://73.241.109.34:8554/unicast \
+#-c:v libvpx -threads 1 -speed 1 -pix_fmt yuv420p -b:v 512k -r 15 \
+       #-f ffm http://127.0.0.1:8090/video.ffm
+#ffmpeg -loglevel info -stimeout 5000000 -rtsp_transport tcp -i rtsp://73.241.109.34:8554/unicast \
+#        -c:v libvpx -threads 1 -speed 6 -pix_fmt yuv420p -async 1 -vsync 1 \
+#       http://127.0.0.1:8090/video.ffm
+ffmpeg -loglevel info -stimeout 5000000 -rtsp_transport tcp -i rtsp://73.241.109.34:8554/unicast \
+        -c:v libx264 -threads 1 -speed 6 -pix_fmt yuv420p -async 1 -vsync 1 \
+     -f ffm  http://127.0.0.1:8090/video.ffm
+
