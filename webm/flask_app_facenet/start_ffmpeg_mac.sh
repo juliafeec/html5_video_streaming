db0c9e25664a09420b11f147b3fe8f37da5da3cb
Julia <julia.feec@gmail.com>
2019-04-11 20:50:16 -0700

    delete v0.1

--- webm/flask_app_facenet/start_ffmpeg_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ffmpeg -f avfoundation -framerate 30 -video_size 1280x720 -i "0" \
-       -f lavfi -i sine \
-       -c:v libvpx -threads 4 -speed 6 -pix_fmt yuv420p -b:v 512k -r 30 \
-       -c:a libopus -threads 4 -b:a 256k -ac 2 -ar 48000 \
-       -vf scale='iw/2:ih/2' \
-       -f ffm http://127.0.0.1:8090/video.ffm
--- webm/flask_app_overlay/start_ffmpeg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ffmpeg -f v4l2 -i /dev/video0 \
-       -f lavfi -i sine \
-       -c:v libvpx -threads 4 -speed 6 -pix_fmt yuv420p -b:v 512k -r 30 \
-       -c:a libopus -threads 4 -b:a 256k -ac 2 -ar 48000 \
-       -f ffm http://127.0.0.1:8090/video.ffm
